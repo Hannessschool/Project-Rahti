6c028c360062ce26855241cda2f1d7d8f5d726ad
Hannessschool <eerolaha@arcada.fi>
2026-03-30 02:46:51 +0300

    FastAPI update

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return { "msg": "Hello!", "v": "0.1" }
+@app.get("/api/ip")
+async def get_ip(request: Request):
+    client_ip = request.client.host
+    return { "ip": client_ip}
 
 
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+@app.get("/ip", response_class=HTMLResponse)
+async def get_ip_html(request: Request):
+    client_ip = request.client.host
+    return f"<h1> Din publika IP-adress är {client_ip} </h1>"
