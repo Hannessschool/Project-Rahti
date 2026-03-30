07565ca6a5e6320a4917a8b0e783ce6649c078d3
Hannessschool <eerolaha@arcada.fi>
2026-03-30 10:29:31 +0300

    connected with frontend

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/api/ip")
 async def get_ip(request: Request):
@@ -14,3 +25,7 @@ async def get_ip_html(request: Request):
     client_ip = request.client.host
     return f"<h1> Din publika IP-adress är {client_ip} </h1>"
 
+
+@app.get("/hello")
+def hello():
+    return {"msg": f"Morjens Doris"} 
