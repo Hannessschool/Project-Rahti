584de16c6a8825f19313617c89f092ba0103b9e8
Hannessschool <eerolaha@arcada.fi>
2026-04-01 10:25:56 +0300

    fixed hotel booking in front-end

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
-import json
-import os
+#import json
+#import os
 
 
 ###CC: What Is My IP
@@ -16,6 +16,16 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+###tillfällig databas, lösning för hotellbokning 0.1
+
+temp_rooms = [
+    { "room_id": 101, "room_name": "Double room", "price": 100},
+    { "room_id": 101, "room_name": "Single room", "price": 70},
+    { "room_id": 101, "room_name": "Suite", "price": 150}
+    ]
+
+
 
 @app.get("/api/ip")
 async def get_ip(request: Request):
@@ -36,11 +46,25 @@ def hello():
 
 
 ###CC: Hotellbokning 0.1
-@app.get("/rooms")
-def get_rooms(request: Request):
-    json_path = os.path.join(os.path.dirname(__file__), "rooms.json")
+#@app.get("/rooms")
+#def get_rooms(request: Request):
+    #json_path = os.path.join(os.path.dirname(__file__), "rooms.json")
     
-    with open(json_path, "r", encoding="utf-8") as file:
-        data = json.load(file)
+    #with open(json_path, "r", encoding="utf-8") as file:
+        #data = json.load(file)
 
-    return data
+    #return data
+
+@app.get("/")
+def read_root():
+    return { "msg": "Welcome to the hotell booking API"}
+
+##alternativ lösning
+@app.get("/rooms")
+def rooms():
+    return temp_rooms
+
+
+@app.post("/bookings")
+def create_booking():
+    return{"msg: Bokning skapad"}  ##skapa bokningar i databasen
