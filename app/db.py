16f1c81bfda156b2bc0ae07771b25ff7e458449d
Hannessschool <eerolaha@arcada.fi>
2026-04-01 12:57:36 +0300

    database work

--- app/db.py
+++ app/db.py
@@ -29,3 +29,36 @@ def create_schema():
                 ALTER TABLE foo ADD COLUMN IF NOT EXISTS name VARCHAR;
                 ALTER TABLE foo ADD COLUMN IF NOT EXISTS created_at TIMESTAMP DEFAULT now()""")
 
+            cur.execute("""
+                -- sample parent table
+                CREATE TABLE IF NOT EXISTS hotel_rooms (
+                    room_id SERIAL PRIMARY KEY,
+                    room_number INT NOT NULL, 
+                    room_name VARCHAR(50),
+                    price DECIMAL(10,2) NOT NULL,
+                    created_at TIMESTAMP DEFAULT now()
+                );        
+                        """)
+
+def create_room_scheme():
+     with get_conn() as conn, conn.cursor() as cur:
+            # Create the schema
+            cur.execute("""
+                -- sample parent table
+                CREATE TABLE IF NOT EXISTS hotel_rooms (
+                    room_id SERIAL PRIMARY KEY, 
+                    room_name TEXT NOT NULL,
+                    price DECIMAL(10,2) NOT NULL,
+                    created_at TIMESTAMP DEFAULT now()
+                );
+                
+                -- sample child table
+                CREATE TABLE IF NOT EXISTS bar (
+                    id SERIAL PRIMARY KEY,
+                    foo_id INT REFERENCES foo(id), -- foreign key
+                    created_at TIMESTAMP DEFAULT now()
+                );
+
+                -- adding columns after the fact
+                ALTER TABLE foo ADD COLUMN IF NOT EXISTS name VARCHAR;
+                ALTER TABLE foo ADD COLUMN IF NOT EXISTS created_at TIMESTAMP DEFAULT now()""")
